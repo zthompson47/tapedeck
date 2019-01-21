15db833fcb9bca4d59f25417f1b4c2e680adb555
Zach Thompson <zach@allotropic.com>
2019-01-21 07:47:52 -0500

    Test streaming from multiple sources

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,8 +1,6 @@
 """Test the pre-configured commands."""
 import os
 
-import trio
-
 from reel import cmd
 from reel.cmd import ffmpeg, play
 
@@ -14,16 +12,31 @@ async def test_cmd_module():
 
 
 async def test_play_radio():
-    """Play a few seconds of Groove Salad."""
+    """Play a few seconds of music."""
     radio = 'http://ice1.somafm.com/groovesalad-256-mp3'
-    with trio.move_on_after(5):
-        async with await ffmpeg.stream(radio) as src:
-            async with await play.speaker() as out:
+    music = ''.join(['https://archive.org/download/',
+                     'gd1977-05-08.shure57.stevenson.29303.flac16/',
+                     'gd1977-05-08d02t04.flac'])
+    async with await play.speaker() as out:
+        async with await ffmpeg.stream(music) as src:
+            count = 0
+            chunk = await src.receive_some(4096)
+            while chunk:
+                if os.environ.get('REEL_ALLOW_AUDIO_PLAY'):
+                    await out.send_all(chunk)
+                if count > 200:
+                    break
                 chunk = await src.receive_some(4096)
-                while chunk:
-
-                    if os.environ.get('REEL_ALLOW_AUDIO_PLAY'):
-                        await out.send_all(chunk)
-
-                    chunk = await src.receive_some(4096)
-                    assert chunk
+                count += 1
+                assert chunk
+        async with await ffmpeg.stream(radio) as src:
+            count = 0
+            chunk = await src.receive_some(4096)
+            while chunk:
+                if os.environ.get('REEL_ALLOW_AUDIO_PLAY'):
+                    await out.send_all(chunk)
+                if count > 200:
+                    break
+                chunk = await src.receive_some(4096)
+                count += 1
+                assert chunk
