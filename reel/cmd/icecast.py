2670243b5624af3c43afed353a15ab13687a21cf
Zach Thompson <zach@allotropic.com>
2019-03-07 20:32:23 -0500

    Add useful defaults for icecast

--- reel/cmd/icecast.py
+++ reel/cmd/icecast.py
@@ -1,20 +1,39 @@
 """The Icecast streaming server."""
-# import reel
+from reel.config import get_xdg_config_dir, get_xdg_data_dir, get_config
+
+from .._spool import Spool
 
 
-# class Icecast:
-#     """A streaming server."""
-#
-#     def __init__(self, host, port, mount, password):
-#         """Initialize an icecast subprocess."""
-#         # self._exe = 'icecast'
-#         # self._flags = ['-c', '{config_file}']
+async def server():
+    """Return a command to run an icecast server."""
+    config_icecast = dict(
+        location='Neptune',
+        admin_email='sushi@trident.sea',
+        password='hack-it-up',
+        hostname='127.0.0.1',
+        port='8777',
+        logdir=str(await get_xdg_data_dir()),
+    )
+    config_dir = await get_xdg_config_dir()
+    config = await get_config(config_dir, 'icecast.xml', **config_icecast)
+    flags = ['-c', str(config)]
+    return Spool('icecast', xflags=flags)
 
-# async with Icecast() + Aria2() + Postgres() as server:
-#     async with playlist | out as player:
-#         async with trio.open_nursery() as nursery:
-#             await server.start_daemon(nursery)
-#             await player.start_daemon(nursery)
-#             # loop & serve
-#             await player.stop()
-#             await server.stop()
+
+def client(host='127.0.0.1', port='8777', mount='asdf', password='hack-it-up'):
+    """Return a command to send a stream to an icecast server."""
+    cmd = 'ffmpeg'
+    flags = [
+        '-re',
+        '-ac', '2',
+        '-ar', '44.1k',
+        '-f', 's16le',
+        '-i', '-',
+        '-vn',
+        '-codec:a', 'libvorbis',
+        '-q:a', '8.0',
+        '-content_type', 'audio/ogg',
+        '-f', 'ogg',
+        f'icecast://source:{password}@{host}:{port}/{mount}'
+    ]
+    return Spool(cmd, xflags=flags)
