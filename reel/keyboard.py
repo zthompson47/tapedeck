530ea121cbededc0a23b06d27217afa0ca185e98
Zach Thompson <zach@allotropic.com>
2019-03-12 07:56:26 -0400

    Finish sync/async integration

--- reel/keyboard.py
+++ reel/keyboard.py
@@ -5,7 +5,6 @@ import io
 import logging
 import os
 import pty
-from select import select
 import sys
 import termios
 import time
@@ -15,157 +14,183 @@ import trio
 
 LOG = logging.getLogger(__name__)
 
-K_UP = -1
-K_DOWN = -2
-K_LEFT = -3
-K_RIGHT = -4
+KEY_EOT = b'\x04'
+KEY_ESC = b'\x1b'
+
+KEY_UP = b'\x1b[A'
+KEY_DOWN = b'\x1b[B'
+KEY_RIGHT = b'\x1b[C'
+KEY_LEFT = b'\x1b[D'
 
 
 class Keyboard:
-    """A way to get keyboard input, Posix-style."""
+    """A way to get keyboard input on Posix systems."""
 
     def __init__(self):
         """Store the term settings before going into cbreak mode."""
-        self._async_stdin = None
-        self._buff = bytearray()
+        self._break_on_eot = None
         self._stashed_term = None
 
-    def __aiter__(self):
-        """Return iterator object."""
-        self._async_stdin = trio.wrap_file(sys.stdin)
-        return self
-
-    async def next(self):
-        """."""
-        return await self.__anext__()
-
-    async def __anext__(self):
-        """Return the next item."""
-        return await self._async_stdin.read(1)
-
-    async def __aenter__(self):
-        """."""
-        self._stash_term()
-        return self
-
-    def __enter__(self):
-        """Save the current term and create a new one with no echo."""
-        self._stash_term()
-
-        # Set stdin file descriptor to non-blocking mode.
+        # Set stdin file descriptor to non-blocking mode
         fileno = sys.stdin.fileno()
         flag = fcntl.fcntl(fileno, fcntl.F_GETFL)
         fcntl.fcntl(fileno, fcntl.F_SETFL, flag | os.O_NONBLOCK)
 
-        # Buffer any existing data from this fd.
-        self._buffer_input()
+    async def __aenter__(self):
+        """Alias the sync func."""
+        return self.__enter__()
 
+    def __enter__(self):
+        """Save the current term and create a new one with no echo."""
+        try:
+            self._stashed_term = termios.tcgetattr(sys.stdin)
+            tty.setcbreak(sys.stdin, termios.TCSANOW)
+        except termios.error as error:
+            LOG.exception(error)
         return self
 
-    def _stash_term(self):
-        """Save the current term and create a new one with no echo."""
-        self._stashed_term = termios.tcgetattr(sys.stdin)
-        tty.setcbreak(sys.stdin, termios.TCSANOW)
-
     async def __aexit__(self, *args):
-        """."""
+        """Bring back the original term settings."""
         self.__exit__(args)
 
     def __exit__(self, *args):
         """Bring back the original term settings."""
-        termios.tcsetattr(sys.stdin, termios.TCSANOW, self._stashed_term)
+        if self._stashed_term:
+            termios.tcsetattr(
+                sys.stdin,
+                termios.TCSANOW,
+                self._stashed_term
+            )
 
-    def _buffer_input(self):
-        """Read as much data as we can from stdin and store in a buffer."""
-        while True:
-            result = sys.stdin.read(1024)
-            if not result:
-                break
-            self._buff.extend(result.encode('utf-8'))
+    def __aiter__(self):
+        """Return iterator object."""
+        return self
 
-    def has_input(self):  # pylint: disable=no-self-use
-        """Check if there is data in the buffer or on the wire."""
-        if self._buff:
-            return True
+    async def __anext__(self):
+        """Iterate over the input streamReturn the next character."""
+        with block_stdin(True):
+            char = await trio.run_sync_in_worker_thread(
+                self.read,
+                cancellable=True
+            )
+        if char == self._break_on_eot:
+            raise StopAsyncIteration
+        return char
 
-        rlist, _wlist, _xlist = select([sys.stdin], [], [], 0)
-        if rlist:
-            return True
+    def break_on_eot(self, character=KEY_EOT):
+        """Set an eot character to break from an iterator."""
+        self._break_on_eot = character
 
-        return False
+    def read(self):  # pylint: disable=no-self-use
+        """Read a unicode character from stdin.
 
-    def get_char(self):  # pylint: disable=no-self-use
-        """Read a unicode character from stdin."""
-        char = None
-        self._buffer_input()
-        if self._buff:
-            char = bytes(chr(self._buff.pop(0)).encode('utf-8'))
-        if not char:
-            return None
+        https://en.wikipedia.org/wiki/ANSI_escape_code#Escape_sequences
+        """
+        char = sys.stdin.read(1)
+        if char != '' and ord(char) == ord(KEY_ESC):
 
-        # Arrow keys
-        if ord(char) == 27:
-            if len(self._buff) == 2:
-                next_two = self._buff[0:2]
-                self._buff = self._buff[2:]
-                if next_two[0] == 91:
-                    if next_two[1] == 68:
-                        char = K_LEFT
-                    elif next_two[1] == 67:
-                        char = K_RIGHT
-                    elif next_two[1] == 66:
-                        char = K_DOWN
-                    elif next_two[1] == 65:
-                        char = K_UP
-            return char
-        return char.decode('utf-8')
+            with block_stdin(False):
+                csi = sys.stdin.read(1)
+                if csi == '[':
+                    ctl = sys.stdin.read(1)
+                    if ctl == 'A':
+                        return KEY_UP
+                    if ctl == 'B':
+                        return KEY_DOWN
+                    if ctl == 'C':
+                        return KEY_RIGHT
+                    if ctl == 'D':
+                        return KEY_LEFT
 
-    def drain(self):
-        """Read all we can into the buffer and throw it away."""
-        self._buffer_input()
-        self._buff.clear()
+            return KEY_ESC
+        return char
+
+
+@contextmanager
+def block_stdin(block: bool) -> None:
+    """Toggle stdin blocking mode."""
+    fileno = sys.stdin.fileno()
+    flag = fcntl.fcntl(fileno, fcntl.F_GETFL)
+
+    if block:
+        fcntl.fcntl(fileno, fcntl.F_SETFL, flag | ~os.O_NONBLOCK)
+    else:
+        fcntl.fcntl(fileno, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+
+    yield
+
+    fcntl.fcntl(fileno, fcntl.F_SETFL, flag)
 
 
 @contextmanager
 def pty_stdin(preload=None):
-    """Replace stdin with a pty."""
+    """Replace stdin with a pty.
+
+    Returns:
+        A function to write data into stdin.
+
+    Examples:
+        >>> with pty_stdin('ab') as stdin:
+        ...     with Keyboard() as keyboard:
+        ...         assert keyboard.read() == 'a'
+        ...         assert keyboard.read() == 'b'
+        ...         stdin('x')
+        ...         assert keyboard.read() == 'x'
+
+    """
     _stashed_stdin = sys.stdin
     master, slave = pty.openpty()
     sys.stdin = io.TextIOWrapper(open(slave, 'rb'))
 
     def stdin(text):
-        """Write data to pty and return bytes written."""
-        LOG.debug('writing %s', text)
+        """Write data to the master side of the pty.
+
+        Returns:
+            Number of bytes written.
+
+        Examples:
+            >>> with pty_stdin() as stdin:
+            ...   assert(stdin('a') == 1)
+
+        """
         if isinstance(text, str):
             return os.write(master, str.encode(text))
         return os.write(master, text)
+
     if preload:
         bytes_written = stdin(preload)
         LOG.debug('bytes: %d', bytes_written)
 
     yield stdin
 
-    # Put stdin back.
     sys.stdin = _stashed_stdin
 
 
 def main():
-    """Test the keyboard out in a polling loop."""
+    """Test the keyboard with a polling loop."""
     with Keyboard() as keyboard:
         while True:
             time.sleep(0.01)
-            if keyboard.has_input():
-                char = keyboard.get_char()
-                if char == 'q':
-                    break
-                # print(chr(ord(char) + 1), end='')
-                print(char, end='')
-                sys.stdout.flush()
-    print('\nexiting in a few seconds...')
-    for _ in range(0, 10):
-        # should be out of cbreak mode now
-        time.sleep(0.3)
+            char = keyboard.read()
+            if char == 'q':
+                break
+            print(char, end='')
+            sys.stdout.flush()
 
+    print('\n')
+    print('Exiting in a few seconds, term should be back to normal...')
+    time.sleep(3)
+
+
+async def amain():
+    """Test the keyboard with a polling loop, async."""
+    async with Keyboard() as keyboard:
+        keyboard.break_on_eot('q')
+        async for key in keyboard:
+            print(key, end='')
+            sys.stdout.flush()
+        print('')
 
 if __name__ == '__main__':
-    main()
+    # main()
+    trio.run(amain)
--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -3,10 +3,11 @@ import logging
 
 from reel.keyboard import (
     Keyboard,
-    K_LEFT, K_RIGHT, K_UP, K_DOWN,
+    KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, KEY_ESC,
     pty_stdin
 )
 
+logging.basicConfig(filename='/Users/zach/kb.log', level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
 
 
@@ -14,36 +15,26 @@ def test_keyboard_context():
     """Open a keyboard and write some characters to it."""
     with pty_stdin('abc' * 1024) as stdin:
         with Keyboard() as keyboard:
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'a'
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'b'
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'c'
+            assert keyboard.read() == 'a'
+            assert keyboard.read() == 'b'
+            assert keyboard.read() == 'c'
 
-            stdin('x')  # goes to end of buffer
+            stdin('x')  # Goes to end of buffer
 
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'a'
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'b'
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'c'
-
+            # Drain buffer
+            assert keyboard.read() == 'a'
+            assert keyboard.read() == 'b'
+            assert keyboard.read() == 'c'
             for _ in range(1018):
-                assert keyboard.has_input()
-                assert keyboard.get_char()
-            assert keyboard.get_char() == 'x'
+                assert keyboard.read()
 
-            keyboard.drain()
+            assert keyboard.read() == 'x'
 
             stdin('de')
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'd'
-            assert keyboard.has_input()
-            assert keyboard.get_char() == 'e'
+            assert keyboard.read() == 'd'
+            assert keyboard.read() == 'e'
 
-            assert not keyboard.has_input()
+            assert keyboard.read() == ''
 
 
 def test_arrows():
@@ -52,32 +43,28 @@ def test_arrows():
     k_right = bytearray([27, 91, 67])
     k_down = bytearray([27, 91, 66])
     k_up = bytearray([27, 91, 65])
+    k_esc = bytearray([27])
     with pty_stdin(k_left) as stdin:
         with Keyboard() as keyboard:
-            assert keyboard.has_input()
-            assert keyboard.get_char() == K_LEFT
-
+            assert keyboard.read() == KEY_LEFT
             stdin(k_right)
-            assert keyboard.has_input()
-            assert keyboard.get_char() == K_RIGHT
-
+            assert keyboard.read() == KEY_RIGHT
             stdin(k_down)
-            assert keyboard.has_input()
-            assert keyboard.get_char() == K_DOWN
-
+            assert keyboard.read() == KEY_DOWN
             stdin(k_up)
-            assert keyboard.has_input()
-            assert keyboard.get_char() == K_UP
+            assert keyboard.read() == KEY_UP
+            stdin(k_esc)
+            assert keyboard.read() == KEY_ESC
 
 
 async def test_keyboard_context_async():
     """The keyboard can be opened as a channel."""
-    message = ['O', 'n', 'c', 'e', ' ', 'u', 'p']
+    message = ['O', 'n', 'c', 'e', ' ', 'u', 'p', 'o']
+    LOG.debug(message)
     with pty_stdin(''.join(message)) as stdin:
         async with Keyboard() as keyboard:
+            keyboard.break_on_eot('o')
             async for key in keyboard:
                 assert key == message.pop(0)
-                if not message:
-                    stdin('q')
-                    assert await keyboard.next() == 'q'
-                    break
+            stdin('a')
+            assert keyboard.read() == 'a'
