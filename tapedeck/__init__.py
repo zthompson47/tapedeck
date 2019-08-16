e9a5605723e00d82d5458ae4fa1b879fa94f9df5
Zach Thompson <zach@allotropic.com>
2019-08-16 06:32:01 -0400

    Hardcode version for failing CI test

--- tapedeck/__init__.py
+++ tapedeck/__init__.py
@@ -1,5 +1,5 @@
 """Tapedeck finds and plays music across muiltiple sources and devices."""
 import pkg_resources as _pkg
-__version__ = _pkg.get_distribution(__name__).version
+__version__ = "0.0.5"  # _pkg.get_distribution(__name__).version
 
 from . import cli
