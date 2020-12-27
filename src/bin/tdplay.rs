7f78bb11b916103fc1a68bd2c47faba7d6ba7cba
Zach Thompson <zach@allotropic.com>
2020-12-27 15:17:54 -0500

    Add logging

--- src/bin/tdplay.rs
+++ src/bin/tdplay.rs
@@ -1,5 +1,16 @@
-use std::process::{Command, Stdio};
+use crossterm::terminal;
+use log::trace;
+use smol::{
+    self, prelude::*,
+    channel::{Receiver, Sender},
+    process::{Command, Stdio},
+};
+use std::panic;
 use structopt::StructOpt;
+use tracing::{info, /*trace,*/ Level};
+use tracing_subscriber::FmtSubscriber;
+
+type Result = std::result::Result<(), String>;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -7,19 +18,106 @@ struct Cli {
     fname: std::path::PathBuf,
 }
 
-fn main() {
+fn with_raw_mode(f: impl FnOnce() + panic::UnwindSafe) -> Result {
+    panic::set_hook(Box::new(|_| {
+        // Do nothing, overriding console error message from panic
+    }));
+
+    terminal::enable_raw_mode().expect("barf enable raw mode");
+    let result = panic::catch_unwind(|| {
+        f();
+    });
+    terminal::disable_raw_mode().expect("barf disable raw mode");
+
+    match result {
+        Ok(_) => Ok(()),
+        Err(err) => panic::resume_unwind(err),
+    }
+}
+
+fn init_logging() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("problem setting global logger");
+
+    info!("Logging initialized...");
+    trace!("[trace] Logging initialized...");
+}
+
+fn main() -> Result {
+    init_logging();
+
     let args = Cli::from_args();
-    let fname = args.fname.to_str().unwrap();
+    let (msg_out, msg_in) = smol::channel::unbounded();
 
-    println!("Playing {}", fname);
+    with_raw_mode(|| {
+        smol::spawn(quit(msg_out)).detach();
+        smol::block_on(play(args.fname, msg_in)).expect("block_on failed");
+    })
+}
 
+async fn play(fname: std::path::PathBuf, msg_in: Receiver<&str>) -> Result {
+    trace!("ENTER player");
     let mut cmd = Command::new("mplayer")
         .arg("-playlist")
         .arg(fname)
+        .stdin(Stdio::null())
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
         .expect("mplayer won't start");
+    trace!("..player: started mplayer");
 
-    cmd.wait().expect("command wasn't running");
+    //rcv_quit.next().await;
+    let result = msg_in.recv().await;
+    trace!("<><><><><><>BEFORE<><><><><><><>");
+    match result {
+        Ok(_) => trace!(
+            "..player>>{}<<: GOT QUIT SIG -->{:?}<--",
+            cmd.id(),
+            result,
+        ),
+        Err(err) => trace!("------->>ERR:{:?}", err),
+    }
+    trace!("<><><><><><>AFTER<><><><><><><>");
+    //trace!("..player: GOT QUIT SIG -->{}<--");
+
+    unsafe {
+        libc::kill(cmd.id() as i32, libc::SIGTERM);
+    };
+    //cmd.kill().expect("can't kill process");
+
+    if let Err(msg) = cmd.status().await {
+        Err(msg.to_string())
+    } else {
+        Ok(())
+    }
+}
+
+async fn quit(msg_out: Sender<&str>) -> Result {
+    trace!("ENTER quitter");
+    let mut stdin = smol::Unblock::new(std::io::stdin());
+    let mut buf = [0; 1];
+
+    loop {
+        trace!("..quitter: waiting for input");
+        let c = stdin.read(&mut buf).await
+            .expect("can't read stdin");
+        trace!("..quitter: GOT input! {}", c);
+        assert!(c == 1);
+        match buf[0] {
+            113 => {
+                trace!("Got QUIT signal");
+                //to_quit.send(42);
+                msg_out.try_send("42").ok();
+                break;
+            },
+            _ => continue,
+        }
+    }
+
+    Ok(())
 }
---
+++ src/logging.rs
@@ -0,0 +1,83 @@
+use std::io::Write;
+use std::sync::Mutex;
+use std::sync::mpsc;
+use log::{
+    log, info, debug, warn, error, trace,
+    Level, LevelFilter, Metadata, Record,
+};
+
+#[derive(Debug)]
+pub struct LogRecord {
+    meta_level: String,
+    pub meta_target: String,
+    module_path: String,
+    file: String,
+    args: String,
+    line: u32,
+}
+
+#[derive(Debug)]
+pub struct Logger {
+    level: LevelFilter,
+    snd: Mutex<mpsc::Sender<LogRecord>>,
+}
+
+impl Logger {
+    pub fn init() {
+        let (snd, rcv) = mpsc::channel();
+        let logger = Logger {
+            level: match std::env::var("RUST_LOG") {
+                Ok(level) => match level.as_str() {
+                    "error" => LevelFilter::Error,
+                    "warn" => LevelFilter::Warn,
+                    "info" => LevelFilter::Info,
+                    "debug" => LevelFilter::Debug,
+                    "trace" => LevelFilter::Trace,
+                    _ => LevelFilter::Info,
+                },
+                _ => LevelFilter::Info,
+            },
+            snd: Mutex::new(snd),
+        };
+        log::set_boxed_logger(Box::new(logger)).expect("already set logger");
+        log::set_max_level(LevelFilter::Trace);
+
+        // Log to stdout
+        std::thread::spawn(move || {
+            while let Ok(msg) = rcv.recv() {
+                //if msg.meta_target == "tdplay" {
+                    write!(
+                        std::io::stdout(),
+                        "{:?}\r\n", msg
+                    ).expect("stdout write error");
+                //}
+            }
+        });
+
+        log!(target: "buttmonkey", Level::Error, "-------------!!!!!!!!! {} error starting", 4);
+        error!("-------------!!!!!!!!! {} error starting", 4);
+        warn!("-------------!!!!!!!!! {} warn starting", 4);
+        info!("-------------!!!!!!!!! {} info starting", 4);
+        debug!("-------------!!!!!!!!! {} debug starting", 4);
+        trace!("-------------!!!!!!!!! {} trace starting", 4);
+    }
+}
+
+impl log::Log for Logger {
+    fn enabled(&self, _metadata: &Metadata) -> bool { true }
+
+    fn log(&self, record: &Record) {
+        self.snd.lock().unwrap().send(
+            LogRecord {
+                meta_level: record.level().to_string(),
+                meta_target: record.target().to_string(),
+                module_path: record.module_path().unwrap().to_string(),
+                file: record.file().unwrap().to_string(),
+                args: record.args().to_string(),
+                line: record.line().unwrap(),
+            }
+        ).expect("channel send error");
+    }
+
+    fn flush(&self) {}
+}
