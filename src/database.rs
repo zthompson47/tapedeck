3feea511dde882c0521b68acf6e90b1ad910e777
Zach Thompson <zach@allotropic.com>
2021-03-22 19:58:28 -0400

    Remove unneeded static

--- src/database.rs
+++ src/database.rs
@@ -1,18 +1,9 @@
 use std::{env, path::PathBuf};
 
-use sqlx::{
-    migrate::{MigrateDatabase, Migrator},
-    sqlite::SqlitePool,
-    Pool, Sqlite,
-};
+use sqlx::{migrate::MigrateDatabase, sqlite::SqlitePool, Pool, Sqlite};
 use tokio::fs;
-use tracing::debug;
 
-pub static MIGRATOR: Migrator = sqlx::migrate!();
-
-/// Find an appropriate place for the database and make sure it exists there.
-///
-/// Return a database pool.
+/// Prepare the database and return a handle.
 pub async fn get_database(app_name: &str) -> Result<Pool<Sqlite>, anyhow::Error> {
     // Look for APPNAME_DEV_DIR environment variable to override default
     let mut dev_dir = app_name.to_uppercase();
@@ -36,7 +27,7 @@ pub async fn get_database(app_name: &str) -> Result<Pool<Sqlite>, anyhow::Error>
     // Make sure the directory exists
     fs::create_dir_all(&dir).await?;
 
-    debug!("Created db dir: {:?}", dir.to_str());
+    tracing::debug!("Created db dir: {:?}", dir.to_str());
 
     // Compose connection string
     let mut url = dir.clone().join(app_name);
@@ -44,14 +35,15 @@ pub async fn get_database(app_name: &str) -> Result<Pool<Sqlite>, anyhow::Error>
     let mut conn_str = String::from("sqlite:");
     conn_str.push_str(url.to_str().unwrap_or(":memory:"));
 
-    debug!("Connection str: {:?}", &conn_str);
+    tracing::debug!("Connection str: {:?}", &conn_str);
 
     // Create if it doesn't exist
     Sqlite::create_database(&conn_str).await?;
 
     // Connect and migrate
     let db = SqlitePool::connect(&conn_str).await?;
-    MIGRATOR.run(&db).await?;
+    let migrator = sqlx::migrate!();
+    migrator.run(&db).await?;
 
     Ok(db)
 }
